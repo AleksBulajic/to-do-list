5641a4cda261282da4ef33831cc132ff2f13d795
AleksBulajic <aleks.bulajic25@gmail.com>
2023-12-11 14:12:41 -0500

    added some css to set the content oin the center of the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
-// import AddTodo from './components/AddTodo'; 
+import AddTodo from './components/AddTodo'; 
 import List from './components/List';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddTodo/>
       <List />
     </div>
   );
